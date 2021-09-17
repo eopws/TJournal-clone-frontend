071b854013c096769eee924216c2f6a17428dc68
xareyli <xareyli@gmail.com>
2021-09-17 20:45:50 +0300

    Added refresh token feature

--- api/axios.ts
+++ api/axios.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+import { IAuthResponse } from '@models/index';
+
 const API_URL = 'http://localhost:3001';
 
 const $api = axios.create({
+    withCredentials: true,
     baseURL: API_URL
 });
 
@@ -11,5 +14,28 @@ $api.interceptors.request.use((config) => {
 
     return config;
 });
+
+$api.interceptors.response.use(
+    (conifg) => {
+        return conifg;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response.status === 401 && error.config && !error.config._isRetry) {
+            originalRequest._isRetry = true;
+
+            try {
+                const response = await axios.post<IAuthResponse>(`${API_URL}/auth/refresh`, undefined, {withCredentials: true});
+
+                localStorage.setItem('token', response.data.accessToken);
+
+                return $api.request(originalRequest);
+            } catch (e) {
+                console.log('unauthorized');
+            }
+        }
+    }
+);
 
 export default $api
